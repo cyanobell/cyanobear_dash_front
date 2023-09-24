199f0ac3b63915bf94528bb2d4d27950cd9dce31
cyanobell <cyanobell@gmail.com>
2023-09-24 22:34:15 +0900

    バグフィックス

--- src/components/GameManager/GameSystem.vue
+++ src/components/GameManager/GameSystem.vue
@@ -4,6 +4,7 @@ import { detect } from "./CollisionDetector"
 import { getData, increaseGameCounter } from "./GameSystemController"
 import { updateDoc, increment } from "@firebase/firestore"
 import cyanobearDashRef from "../../firebase_settings/index"
+import { serverAcccessCount } from './GameDataModel'
 
 
 const doUpdate = () => {
@@ -15,10 +16,11 @@ const doUpdate = () => {
 onMounted(() => {
     doUpdate()
     postWebCount()
-    getData()
 })
-const postWebCount= async () => {
 
+const postWebCount= async () => {
+    await getData()
+    serverAcccessCount.value += 1
     await updateDoc(cyanobearDashRef, {
         web_count: increment(1)
     });
