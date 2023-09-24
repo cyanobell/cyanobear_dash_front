08974e97d6f36c77c0e0ba12d65df861dfaf0f55
cyanobell <cyanobell@gmail.com>
2023-09-24 10:08:26 +0900

    タイトル変更

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    devServer: {port: 5000}
+    devServer: {port: 5000},
+    pages: {
+        index: {
+            entry: 'src/main.ts',
+            title: "シアノベアダッシュ！",
+        }
+    },
 })
